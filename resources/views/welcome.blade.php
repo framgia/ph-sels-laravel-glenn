8832a85cab974e8a778520b0c4d1ee57170483c3
Louille Glen Benatiro <glenbenatiro@gmail.com>
2019-07-05 18:05:12 +0800

    [SELS-TASK] User Registration, Login, and Logout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Framgia E-Learning
                 </div>
 
                 <div class="links">
