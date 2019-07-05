c503b857588be330e1431ee05318a20492eed939
Louille Glen Benatiro <glenbenatiro@gmail.com>
2019-07-05 18:05:12 +0800

    [SELS-TASK] User Dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,9 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <!-- <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1"> -->
 
-    <!-- Required meta tags -->
+<head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -23,15 +20,25 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
 
-                <a class="navbar-brand" href="{{ url('/home') }}">
+                @guest
+                <a class="navbar-brand" href="/">
                     E-Learning System
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                @else
+                <a class="navbar-brand" href="/dashboard">
+                    E-Learning System
+                </a>
+                @endguest
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -45,36 +52,37 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->username }} <span class="caret"></span>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->username }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="/users/{{ Auth::id() }}">
+                                    Profile
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="/users/{{ Auth::id() }}">
-                                        Profile
-                                    </a>
-
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                    {{ __('Logout') }}
+                                </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                    style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
                         @endguest
                     </ul>
                 </div>
@@ -85,5 +93,8 @@
             @yield('content')
         </main>
     </div>
+
+
 </body>
+
 </html>
---
+++ resources/views/layouts/test_activity.blade.php
@@ -0,0 +1,44 @@
+<div class="row pt-4">
+    <div class="col-3">
+        <img src="/uploads/avatars/{{ $user->avatar }}" class="border img-fluid rounded-circle mx-auto d-block"
+            style="max-width: 100px" alt="User Avatar" />
+    </div>
+    <div class="col">
+        <div class="container">
+            <a href="/users/{{ $user->id }}">You</a> learned 20 of 20 words in <a href="#">Basic 500</a>
+        </div>
+        <div class="container">
+            <small class="text-muted">2 days ago</small>
+        </div>
+    </div>
+</div>
+
+<div class="row pt-4">
+    <div class="col-3">
+        <img src="/uploads/avatars/{{ $user->avatar }}" class="border img-fluid rounded-circle mx-auto d-block"
+            style="max-width: 100px" alt="User Avatar" />
+    </div>
+    <div class="col">
+        <div class="container">
+            <a href="/users/{{ $user->id }}">You</a> learned 20 of 20 words in <a href="#">Basic 500</a>
+        </div>
+        <div class="container">
+            <small class="text-muted">2 days ago</small>
+        </div>
+    </div>
+</div>
+
+<div class="row pt-4">
+    <div class="col-3">
+        <img src="/uploads/avatars/{{ $user->avatar }}" class="border img-fluid rounded-circle mx-auto d-block"
+            style="max-width: 100px" alt="User Avatar" />
+    </div>
+    <div class="col">
+        <div class="container">
+            <a href="/users/{{ $user->id }}">You</a> learned 20 of 20 words in <a href="#">Basic 500</a>
+        </div>
+        <div class="container">
+            <small class="text-muted">2 days ago</small>
+        </div>
+    </div>
+</div>
