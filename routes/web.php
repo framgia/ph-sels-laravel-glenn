7df7472dd351f038b8fd309ce3be7996c1983d40
Louille Glen Benatiro <glenbenatiro@gmail.com>
2019-07-10 18:11:56 +0800

    .

--- routes/web.php
+++ routes/web.php
@@ -16,39 +16,6 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 Route::get('/dashboard', 'DashboardController@index')->name('home');
-<<<<<<< HEAD
-=======
-=======
-
->>>>>>> [SELS-TASK] User Profile Page
-Route::get('/home', 'HomeController@index')->name('home');
->>>>>>> [SELS-TASK] User Registration, Login, and Logout
-=======
->>>>>>> [SELS-TASK] User Dashboard
-=======
-Route::view('/', 'welcome');
-Route::get('/dashboard', 'DashboardController@index');
->>>>>>> [SELS-TASK] User Edit Page and Home Page
-=======
-=======
-
->>>>>>> [SELS-TASK] User Profile Page
-Route::get('/home', 'HomeController@index')->name('home');
->>>>>>> [SELS-TASK] User Registration, Login, and Logout
-=======
-Route::get('/dashboard', 'DashboardController@index')->name('home');
->>>>>>> [SELS-TASK] User Dashboard
-=======
-Route::view('/', 'welcome');
-Route::get('/dashboard', 'DashboardController@index');
->>>>>>> [SELS-TASK] User Edit Page and Home Page
 Route::resource('users', 'UsersController');
